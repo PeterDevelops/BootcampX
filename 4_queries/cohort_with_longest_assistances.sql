e7192ee1d2670f9bf5a86f187f0a649d563bbda5
PeterDevelops <PeterDinhBC@gmail.com>
2024-04-07 15:35:39 -0700

    Add query for cohort with the longest assitance

---
+++ 4_queries/cohort_with_longest_assistances.sql
@@ -0,0 +1,11 @@
+-- Get the cohort with the longest average duration of assistance requests.
+-- The same requirements as the previous query, but only return the single row with the longest average.
+SELECT cohorts.name as name, avg(assistance_requests.completed_at - assistance_requests.started_at) as average_assistance_time
+FROM cohorts
+JOIN students
+ON cohorts.id = students.cohort_id
+JOIN assistance_requests
+ON students.id = assistance_requests.student_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time DESC
+LIMIT 1;
